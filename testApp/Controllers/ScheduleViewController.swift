3dc3b4311aa808c56aed83224b0973a44db5bbd6
DariaShantalova <darya.shantalova@phystech.edu>
2022-06-26 20:37:05 +0300

    Added calendar

--- testApp/Controllers/ScheduleViewController.swift
+++ testApp/Controllers/ScheduleViewController.swift
@@ -9,20 +9,29 @@ import UIKit
 import FSCalendar
 
 class ScheduleViewController: UIViewController {
-    
-    private var calendar: FSCalendar = {
-        let calendar = FSCalendar()
-        calendar.translatesAutoresizingMaskIntoConstraints = false
-        return calendar
-    }
+    fileprivate weak var calendar: FSCalendar!
+//    public var calendar: FSCalendar = {
+//        let calendar = FSCalendar()
+//
+//        calendar.translatesAutoresizingMaskIntoConstraints = false
+//        return calendar
+//    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Schedule"
+        let calendar = FSCalendar(frame: CGRect(x: 30, y: 80, width: 320, height: 300))
+        view.addSubview(calendar)
+        
+        self.calendar = calendar
+    
+//        setConstraints()
+       
+        
         // Do any additional setup after loading the view.
     }
     
-   
-
 }
+
+
 
